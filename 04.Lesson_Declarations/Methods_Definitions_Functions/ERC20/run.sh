9813e3b459b15ccc439d1b4a86e0dff316b120cb
Kuldeep K Srivastava <kuldeep@Kuldeeps-MacBook-Pro-2.local>
2022-03-01 00:00:58 +0530

    lesson 4 done

---
+++ 04.Lesson_Declarations/Methods_Definitions_Functions/ERC20/run.sh
@@ -0,0 +1,4 @@
+certoraRun ERC20Fixed.sol:ERC20 --verify ERC20:ERC20.spec \
+--solc ~/solc/0.8.0/solc-macos \
+--optimistic_loop \
+--msg "added method declaration"
---
+++ 04.Lesson_Declarations/Methods_Definitions_Functions/MeetingScheduler/run.sh
@@ -0,0 +1,3 @@
+certoraRun MeetingSchedulerFixed.sol:MeetingScheduler --verify MeetingScheduler:meetings.spec \
+--solc ~/solc/0.8.7/solc-macos \
+--msg "added method declaration"
